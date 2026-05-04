e24ce6dd22afcb3ad3a5c9f66edd76327f64355b
Jesper <jesper.olsson.vxo@gmail.com>
2026-05-04 10:28:26 +0200

    Utvecklingsmiljö

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.json({ ok: true });
+});
+
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello from Express!", time: new Date().toISOString() });
 });
 
 app.listen(3001, "0.0.0.0", () => {
