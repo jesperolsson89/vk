498708900e89aa683254f39511d26f75aa9d08df
Jesper <jesper.olsson.vxo@gmail.com>
2026-06-01 10:23:50 +0200

    .

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Game from "./components/Game";
-// import UserProfile from "./components/UserProfile";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
       <Header />
       <div className="flex flex-col min-h-screen items-center bg-primary-200 pt-16">
-        {/* <UserProfile/> */}
+        <UserProfile/>
         <Game />
       </div>
       <Footer />
